59827b14201858d4230e8f96f91778bf2bbca81e
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2017-03-24 20:50:50 -0700

    Added view for user value confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,8 @@ class UsersController < ApplicationController
      end
    end
 
+   def confirm
+     
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+
   resources :topics do
-     resources :posts, except: [:index]
-   end
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
